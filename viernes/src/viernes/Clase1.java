0d739aed9f71d0e4e2dae3327d26cd25173d14f6
JhonatanGP <jguzman5292@ceuandalucia.es>
2023-11-24 19:27:14 +0100

    subo proyecto con mi nombre

--- viernes/src/viernes/Clase1.java
+++ viernes/src/viernes/Clase1.java
@@ -5,6 +5,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("jhonatan guzmán");
+System.out.println("estoy desarrollando");
 	}
 
 }
